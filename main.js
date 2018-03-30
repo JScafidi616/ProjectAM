a153f4a24a38dc65b4ff6d2cc5c4472c395f01d3
Jose Cartin <jacarve@gmail.com>
2018-03-29 22:55:57 -0600

    Se agrego FontAwesome.

--- main.js
+++ main.js
@@ -7,13 +7,14 @@ let win
 
 
 function createWindow(){
-    win = new BrowserWindow ({width: 800, height:600, frame: false})
+    win = new BrowserWindow ({width: 800, height:600, frame: true})
+    win.setMenu(null);
     win.loadURL(url.format({
         pathname: path.join(__dirname, '\\App\\index.html'),
         protocol: 'file',
         slashes: true
     }))
-    win.webContents.openDevTools()
+    //win.webContents.openDevTools()
 
     console.log("mensaje de consola");
 }
